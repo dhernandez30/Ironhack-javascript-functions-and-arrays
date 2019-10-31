a5d01d4e1077d3c71ebfa085de9c12420f860081
Hernandez Daniel <20167@columbushs.com>
2019-10-31 12:44:17 -0400

    36

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,8 +1,23 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers(num1, num2){
+if (num1 > num2 ) {
+  return num1;
+}
+else {
+  return num2;
+}
+};
+
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
+function findLongestWord (array) {
+  if(array.length ===0) {
+    retun 
+  }
+}
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
